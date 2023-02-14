0a1b0f99ac94fc41c5db465b6d23d44a26ee9306
新島　駿 <niijima131229@docomo.ne.jp>
2023-02-14 20:12:16 +0900

    ログインチェック機能実装

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,22 +1,15 @@
 import axios from 'axios'
 import Config from 'config'
 
-function useAuth(): boolean {
-    const result = refresh()
-    console.log(result)
-    return false
-}
-
-async function refresh(): Promise<boolean> {
-    const url = Config.apiLoginUrl + '/refresh'
+async function useAuth(): Promise<boolean> {
+    const url = Config.apiLoginUrl + '/is-auth'
     const token = localStorage.getItem('token') ?? ''
     const headers = {
         Authorization: `Bearer ${token}`,
     }
 
     try {
-        const response = await axios.post(url, {}, { headers })
-        localStorage.setItem('token', response.data.authorization.token)
+        await axios.post(url, {}, { headers })
         return true
     } catch (error) {
         console.log(error)
